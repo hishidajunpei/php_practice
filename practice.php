bfe6f695ef2a16ee569bc028e6c15041cf7f60e9
j-hishida <1279059@gmail.com>
2022-01-18 14:10:34 +0000

    課題２

--- practice.php
+++ practice.php
@@ -1,18 +1,23 @@
 <?php
 $a = 3;
-$a = $a + 5;
-$a = 3;
 $b = 7;
 
 echo $a + $b;
+
 echo "\n";
-
-$array2 = ["spring", "summer", "autumn", "winter"];
-
 $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
 
 echo $array_month[7];
+
 echo "\n";
+$hello = "Hello,";
+$name = "Junpei";
+$world = "'s World!";
 
+echo $hello. $name. $world;
 
-test
+echo "\n";
+$tech_boost = "tech";
+$tech_boost .= " boost";
+
+echo $tech_boost;
