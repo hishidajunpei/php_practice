02a4d32d71b915b7cd021483ea33f0da217eb6f1
j-hishida <1279059@gmail.com>
2022-01-18 04:57:11 +0000

    課題02

---
+++ practice.php
@@ -0,0 +1,17 @@
+<?php
+$a = 3;
+$a = $a + 5;
+$a = 3;
+$b = 7;
+
+echo $a + $b;
+echo "\n";
+
+$array2 = ["spring", "summer", "autumn", "winter"];
+
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+
+echo $array_month[7];
+echo "\n";
+
+
