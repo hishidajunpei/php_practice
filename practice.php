4d9a13bedf6449a8c08e470e44394f178c1de7aa
j-hishida <1279059@gmail.com>
2022-01-20 13:45:42 +0000

    課題3

--- practice.php
+++ practice.php
@@ -1,43 +1,37 @@
 <?php
-$a = 3;
-$b = 7;
-
-echo $a + $b;
-
-echo "\n";
-$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-
-echo $array_month[7];
-
-echo "\n";
-$hello = "Hello,";
 $name = "Junpei";
-$world = "'s World!";
+switch ($name){
+    case "Junpei";
+    echo "「私はあなたの名前です。」";
+    break;
+    default;
+    echo "「あなたの名前ではありません」";
+}
+echo "\n";
 
-echo $hello. $name. $world;
+$total = 0;
+
+for ($i = 1; $i<=10000; $i++) {
+    $total += $i;
+}
+    echo $total;
 
 echo "\n";
-$tech_boost = "tech";
-$tech_boost .= " boost";
 
-echo $tech_boost;
+$fruits = array("Apple","Orange","Lemon","Grape","Banana");
+foreach($fruits as $fruit){
+echo "要素は" .$fruit;
+}
 
-$calender_2018 = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" =>"6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "12月"
-];
+echo "\n";
 
-// 12月を表示する
-echo $calender_2018[11];
+$start = 1;
+$end = 100;
 
+for($i = $start; $i <= $end; $i++){
 
+if($i % 5 === 0){
+    echo $i;
+    echo "\n";
+  }
+}
